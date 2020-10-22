d3dc5232d82fc187db3c0b8cc5d196d2e8fbf64f
Artyom Okhota <shpager@gmail.com>
2020-10-22 22:21:56 +0300

    Upload project

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _markerDefineStatus;
+    [SerializeField] private GameObject _background;
+
+    public void ShowBackground()
+    {
+        _background.SetActive(true);
+        _markerDefineStatus.SetActive(false);
+    }
+
+    public void HideBackground()
+    {
+        _background.SetActive(false);
+        _markerDefineStatus.SetActive(true);
+    }
+}
